5e3229f3a05f8367842d97ae707fbeefa09234d7
Podkolodnyi <podkolodnyi@gmail.com>
2026-01-30 17:05:49 +0300

    3

--- stud_proj/js/main.js
+++ stud_proj/js/main.js
@@ -1,6 +1,24 @@
 import activeMainMenu from "./components/activeMainMenu.js";
 import activeLocation from "./components/activeLocation.js";
-import getProductCard from "./components/getProductCard.js";
+import getProductCard from "./components/ProductCard.js";
+
+async function getCards() {
+    let response = await fetch('./data/data.json');
+    return await response.json();
+}
+
+function renderCards() {
+    getCards().then((cards) => {
+        cards.forEach((el) => {
+            let catalogList = document.querySelector(".catalog__list");
+            let catalogItem = document.createElement("li");
+            catalogItem.classList.add("catalog__item");
+            catalogList.append(catalogItem);
+            let card = new getProductCard(el);
+            catalogItem.append(card);
+        })
+    })
+}
 
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -8,6 +26,6 @@ window.addEventListener('DOMContentLoaded', () => {
     activeMainMenu();
     //выбрать город
     activeLocation();
-let oko = document.querySelector(".catalog__item2");
-oko.append(getProductCard())
+
+    renderCards();
 });
